6281375d112cfff39a6ca8f4cc220d04fbeba288
SouravW3 <souravp55i@gmail.com>
2026-01-15 22:32:23 -0500

    Edit List 1. Added MPPO_FAM_CAM trained model and logs on V3_Base_Cam. 2. Modified TB_env_FAM_V3_Base_Cam.py to reflect map location.

--- Environments/FAM_CAM/TB_env_FAM_V3_Base_Cam.py
+++ Environments/FAM_CAM/TB_env_FAM_V3_Base_Cam.py
@@ -22,7 +22,7 @@ VEC_SIZE    = VEC_ECOUNT  + 1
 # ───────────────────── Map registration ───────────────────────
 class TwoBridgeMap_V3_Base_Cam(lib.Map):
     name      = "TwoBridgeMap_V3_Base_Cam"
-    directory = r"C:/Program Files (x86)/StarCraft II/Maps/Strategy Maps/Camera Lock/Camera Lock"
+    directory = r"C:/Program Files (x86)/StarCraft II/Maps/Strategy Maps/Camera Lock"
     filename  = "TwoBridgeMap_V3_Base_Cam.SC2Map"
     players   = 2
 
